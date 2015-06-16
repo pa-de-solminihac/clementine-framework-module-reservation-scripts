fce8ab4872e97e021494dc5e9c3e9bd7dc3ec8a0
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-06-16 18:44:48 +0200

    fix script install reservation 1.0

--- versions/1.0/upgrade.php
+++ versions/1.0/upgrade.php
@@ -1,32 +1,4 @@
 <?php
-
-$sql = <<<SQL
-    CREATE TABLE IF NOT EXISTS `clementine_reservation` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `number_people` int(11) DEFAULT NULL,
-      `cancel` tinyint(1) DEFAULT NULL,
-      `comment` longtext,
-      `information_id` int(11) DEFAULT NULL,
-      `start_date` datetime DEFAULT NULL,
-      `end_date` datetime DEFAULT NULL,
-      `user_id` int(11) NOT NULL,
-      PRIMARY KEY(`id`),
-      INDEX `fk_Reservation_user1_idx` (`user_id`),
-      CONSTRAINT `fk_Reservation_User1`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `clementine_reservation_users` (`id`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
-    DEFAULT CHARACTER SET = UTF8;
-SQL;
-
-if (!$db->prepare($sql)->execute()) {
-    $db->rollBack();
-    return false;
-}
-
 $sql = <<<SQL
     CREATE TABLE IF NOT EXISTS `clementine_reservation_horaire` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,8 +13,8 @@ $sql = <<<SQL
       `time_creneaux` time NOT NULL,
       `option` int(11) NOT NULL DEFAULT '0',
       PRIMARY KEY(`id`)
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -61,8 +33,8 @@ $sql = <<<SQL
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `repeat` int(11) DEFAULT NULL,
       PRIMARY KEY(`id`)
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -76,8 +48,34 @@ $sql = <<<SQL
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `libelle` varchar(45) DEFAULT NULL,
       PRIMARY KEY(`id`)
+    ) 
+    ENGINE=InnoDB 
+    DEFAULT CHARACTER SET = UTF8;
+SQL;
+
+if (!$db->prepare($sql)->execute()) {
+    $db->rollBack();
+    return false;
+}
+$sql = <<<SQL
+    CREATE TABLE IF NOT EXISTS `clementine_reservation` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `number_people` int(11) DEFAULT NULL,
+      `cancel` tinyint(1) DEFAULT NULL,
+      `comment` longtext,
+      `information_id` int(11) DEFAULT NULL,
+      `start_date` datetime DEFAULT NULL,
+      `end_date` datetime DEFAULT NULL,
+      `user_id` int(11) NOT NULL,
+      PRIMARY KEY(`id`),
+      INDEX `fk_clementine_reservation_clementine_reservation_user1` (`user_id`),
+      CONSTRAINT `fk_clementine_reservation_clementine_reservation_user1`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `clementine_reservation_users` (`id`) 
+        ON DELETE NO ACTION 
+        ON UPDATE NO ACTION
     )
-    ENGINE=InnoDB
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -91,19 +89,20 @@ $sql = <<<SQL
       `option_id` int(11) NOT NULL AUTO_INCREMENT,
       `clementine_users_privileges_id` int(10) unsigned NOT NULL,
       PRIMARY KEY (`option_id`,`clementine_users_privileges_id`),
-      INDEX `fk_Option2` (`clementine_users_privileges_id`) ,
-      CONSTRAINT `fk_Option1`
-        FOREIGN KEY (`option_id`)
-        REFERENCES `clementine_reservation_option` (`id`)
-        ON DELETE NO ACTION
+      INDEX `fk_clementine_reservation_option_has_privilege_clemenetine_reservation_option1 (option_id)`
+      INDEX `fk_clementine_reservation_option_has_privilege_clementine_users_privileges` (`clementine_users_privileges_id`),
+      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clemenetine_reservation_option1` 
+        FOREIGN KEY (`option_id`) 
+        REFERENCES `clementine_reservation_option` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_Option2`
-        FOREIGN KEY (`clementine_users_privileges_id`)
+      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clementine_users_privileges` 
+        FOREIGN KEY (`clementine_users_privileges_id`) 
         REFERENCES `clementine_users_privileges` (`id`)
-        ON DELETE NO ACTION
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -121,14 +120,14 @@ $sql = <<<SQL
       `libelle` varchar(255) NOT NULL,
       `maximum_number_place_by_reservation` int(11) NOT NULL,
       PRIMARY KEY (`id`),
-      INDEX `fk_Ressource_clementine_users1` (`client_id`),
-      CONSTRAINT `fk_Ressource_clementine_users1`
-        FOREIGN KEY (`client_id`)
-        REFERENCES `clementine_users` (`id`)
-        ON DELETE NO ACTION
+      INDEX `fk_clementine_reservation_ressource_clementine_users1` (`client_id`),
+      CONSTRAINT `fk_clementine_reservation_ressource_clementine_users1` 
+        FOREIGN KEY (`client_id`) 
+        REFERENCES `clementine_users` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 if (!$db->prepare($sql)->execute()) {
@@ -141,20 +140,20 @@ $sql = <<<SQL
       `ressource_id` int(11) NOT NULL,
       `horaire_id` int(11) NOT NULL,
       PRIMARY KEY (`ressource_id`,`horaire_id`),
-      INDEX `fk_Ressource_has_Horaire_horaire1_idx` (`horaire_id`),
-      INDEX `fk_Ressource_has_Horaire_ressource1_idx` (`ressource_id`),
-      CONSTRAINT `fk_Ressource_has_Horaire_Horaire1`
-        FOREIGN KEY (`horaire_id`)
-        REFERENCES `clementine_reservation_horaire` (`id`)
-        ON DELETE NO ACTION
+      INDEX `fk_clementine_reservation_ressource_has_horaire_clementine_reservation_horaire1` (`horaire_id`), 
+      INDEX `fk_clementine_reservation_ressource_has_Horaire_clementine_reservation_ressource1` (`ressource_id`),
+      CONSTRAINT `fk_clementine_reservation_ressource_has_horaire_clementine_reservation_horaire1` 
+        FOREIGN KEY (`horaire_id`) 
+        REFERENCES `clementine_reservation_horaire` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_Ressource_has_Horaire_Ressource1`
-        FOREIGN KEY (`ressource_id`)
-        REFERENCES `clementine_reservation_ressource` (`id`)
-        ON DELETE NO ACTION
+      CONSTRAINT `fk_clementine_reservation_ressource_has_Horaire_clementine_reservation_ressource1` 
+        FOREIGN KEY (`ressource_id`) 
+        REFERENCES `clementine_reservation_ressource` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -169,14 +168,14 @@ $sql = <<<SQL
       `ressource_id` int(11) NOT NULL,
       `primary` tinyint(1) DEFAULT NULL
       PRIMARY KEY (`reservation_id`,`ressource_id`),
-      INDEX `fk_RessourceReserve_Ressource1` (`ressource_id`),
-      CONSTRAINT `fk_RessourceReserve_Reservation1`
-        FOREIGN KEY (`reservation_id`)
-        REFERENCES `clementine_reservation` (`id`)
-        ON DELETE NO ACTION
+      INDEX `fk_clementine_reservation_ressource_has_reservation_clementine_reservation_ressource1` (`ressource_id`),
+      CONSTRAINT `fk_clementine_reservation_ressource_has_reservation_clementine_reservation_ressource1` 
+        FOREIGN KEY (`reservation_id`) 
+        REFERENCES `clementine_reservation` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -193,9 +192,9 @@ $sql = <<<SQL
       `mail` varchar(45) DEFAULT NULL,
       `clementine_users_id` int(10) unsigned DEFAULT NULL,
       PRIMARY KEY (`id`),
-      INDEX `fk_User_UserConnecte1` (`clementine_users_id`)
-    )
-    ENGINE=InnoDB 
+      INDEX `fk_clementine_reservation_user_clementine_reservation_users1` (`clementine_users_id`)
+    ) 
+    ENGINE=InnoDB  
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -209,20 +208,20 @@ $sql = <<<SQL
       `user_id` int(11) NOT NULL,
       `option_id` int(11) NOT NULL,
       PRIMARY KEY (`user_id`,`option_id`),
-      INDEX `fk_Option_has_User_User1_idx` (`user_id`),
-      INDEX `fk_Option_has_User_Option1_idx` (`option_id`),
-      CONSTRAINT `fk_Option_has_User_Option1`
-        FOREIGN KEY (`option_id`)
-        REFERENCES `clementine_reservation_option` (`id`)
-        ON DELETE NO ACTION
+      INDEX `fk_clementine_reservation_users_has_option_clementine_reservation_user1` (`user_id`), 
+      INDEX `fk_clementine_reservation_option_has_user_clementine_reservation_option1` (`option_id`),
+      CONSTRAINT `fk_clementine_reservation_option_has_user_clementine_reservation_option1` 
+        FOREIGN KEY (`option_id`) 
+        REFERENCES `clementine_reservation_option` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_Option_has_User_User1`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `clementine_reservation_users` (`id`)
-        ON DELETE NO ACTION
+      CONSTRAINT `fk_clementine_reservation_users_has_option_clementine_reservation_user1` 
+        FOREIGN KEY (`user_id`) 
+        REFERENCES `clementine_reservation_users` (`id`) 
+        ON DELETE NO ACTION 
         ON UPDATE NO ACTION
-    )
-    ENGINE=InnoDB
+    ) 
+    ENGINE=InnoDB 
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
