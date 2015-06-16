53b5d9a26db0668d5d4131aa585ba64e3c5da15d
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-06-16 19:00:21 +0200

    suppression d'un espace

--- versions/1.0/upgrade.php
+++ versions/1.0/upgrade.php
@@ -68,7 +68,7 @@ $sql = <<<SQL
       PRIMARY KEY (`id`),
       INDEX `fk_clementine_reservation_user_clementine_reservation_users1` (`clementine_users_id`)
     )
-    ENGINE=InnoDB 
+    ENGINE=InnoDB
     DEFAULT CHARACTER SET = UTF8;
 SQL;
 
@@ -109,14 +109,14 @@ $sql = <<<SQL
       `option_id` int(11) NOT NULL AUTO_INCREMENT,
       `clementine_users_privileges_id` int(10) unsigned NOT NULL,
       PRIMARY KEY (`option_id`,`clementine_users_privileges_id`),
-      INDEX `fk_clementine_reservation_option_has_privilege_clemenetine_reservation_option1` (`option_id`),
-      INDEX `fk_clementine_reservation_option_has_privilege_clementine_users_privileges` (`clementine_users_privileges_id`),
-      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clemenetine_reservation_option1`
+      INDEX `fk_clementine_reservation_option_has_privilege_clementine_rese1` (`option_id`),
+      INDEX `fk_clementine_reservation_option_has_privilege_clementine_user2` (`clementine_users_privileges_id`),
+      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clementine_rese1`
         FOREIGN KEY (`option_id`)
         REFERENCES `clementine_reservation_option` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clementine_users_privileges`
+      CONSTRAINT `fk_clementine_reservation_option_has_privilege_clementine_user2`
         FOREIGN KEY (`clementine_users_privileges_id`)
         REFERENCES `clementine_users_privileges` (`id`)
         ON DELETE NO ACTION
@@ -160,14 +160,14 @@ $sql = <<<SQL
       `ressource_id` int(11) NOT NULL,
       `horaire_id` int(11) NOT NULL,
       PRIMARY KEY (`ressource_id`,`horaire_id`),
-      INDEX `fk_clementine_reservation_ressource_has_horaire_clementine_reservation_horaire1` (`horaire_id`),
-      INDEX `fk_clementine_reservation_ressource_has_Horaire_clementine_reservation_ressource1` (`ressource_id`),
-      CONSTRAINT `fk_clementine_reservation_ressource_has_horaire_clementine_reservation_horaire1`
+      INDEX `fk_clementine_reservation_ressource_has_horaire_clementine_res1` (`horaire_id`),
+      INDEX `fk_clementine_reservation_ressource_has_Horaire_clementine_res2` (`ressource_id`),
+      CONSTRAINT `fk_clementine_reservation_ressource_has_horaire_clementine_res1`
         FOREIGN KEY (`horaire_id`)
         REFERENCES `clementine_reservation_horaire` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_clementine_reservation_ressource_has_Horaire_clementine_reservation_ressource1`
+      CONSTRAINT `fk_clementine_reservation_ressource_has_Horaire_clementine_res2`
         FOREIGN KEY (`ressource_id`)
         REFERENCES `clementine_reservation_ressource` (`id`)
         ON DELETE NO ACTION
@@ -188,8 +188,8 @@ $sql = <<<SQL
       `ressource_id` int(11) NOT NULL,
       `primary` tinyint(1) DEFAULT NULL
       PRIMARY KEY (`reservation_id`,`ressource_id`),
-      INDEX `fk_clementine_reservation_ressource_has_reservation_clementine_reservation_ressource1` (`ressource_id`),
-      CONSTRAINT `fk_clementine_reservation_ressource_has_reservation_clementine_reservation_ressource1`
+      INDEX `fk_clementine_reservation_ressource_has_reservation_clementine1` (`ressource_id`),
+      CONSTRAINT `fk_clementine_reservation_ressource_has_reservation_clementine1`
         FOREIGN KEY (`reservation_id`)
         REFERENCES `clementine_reservation` (`id`)
         ON DELETE NO ACTION
@@ -209,14 +209,14 @@ $sql = <<<SQL
       `user_id` int(11) NOT NULL,
       `option_id` int(11) NOT NULL,
       PRIMARY KEY (`user_id`,`option_id`),
-      INDEX `fk_clementine_reservation_users_has_option_clementine_reservation_user1` (`user_id`),
-      INDEX `fk_clementine_reservation_option_has_user_clementine_reservation_option1` (`option_id`),
-      CONSTRAINT `fk_clementine_reservation_option_has_user_clementine_reservation_option1`
+      INDEX `fk_clementine_reservation_users_has_option_clementine_reservat1` (`user_id`),
+      INDEX `fk_clementine_reservation_option_has_user_clementine_reservat2` (`option_id`),
+      CONSTRAINT `fk_clementine_reservation_option_has_user_clementine_reservat2`
         FOREIGN KEY (`option_id`)
         REFERENCES `clementine_reservation_option` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
-      CONSTRAINT `fk_clementine_reservation_users_has_option_clementine_reservation_user1`
+      CONSTRAINT `fk_clementine_reservation_users_has_option_clementine_reservat1`
         FOREIGN KEY (`user_id`)
         REFERENCES `clementine_reservation_users` (`id`)
         ON DELETE NO ACTION
